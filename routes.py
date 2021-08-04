a744af25c0a3850d5867405f793e5093fe29fcfa
root <root@LAPTOP-DBRPH672.localdomain>
2021-08-04 19:12:05 +0300

    Added Procfile, routes and users

---
+++ routes.py
@@ -0,0 +1,64 @@
+from app import app
+from db import db
+from flask import Flask
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+import users, courses, stats
+
+@app.route("/")
+def index():
+    result = db.session.execute("SELECT content FROM messages")
+    messages = result.fetchall()
+    return render_template("index.html", count=len(messages), messages=messages)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO messages (content) VALUES (:content)"
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        if len(username) < 1 or len(username) > 30:
+            return render_template("error.html", message="Käyttäjätunnus on väärän pituinen")
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat ovat erit")
+
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti epäonnistui")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+    if users.login(username, password):
+        return redirect("/")
+    else:
+        return render_template("error.html", message="Väärä käyttäjätunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
---
+++ users.py
@@ -0,0 +1,37 @@
+from db import db
+from flask import abort, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+import os
+
+def login(name, password):
+    sql = "SELECT password, id, role FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            session["user_id"] = user[1]
+            session["user_name"] = name
+            session["csrf_token"] = os.urandom(16).hex()
+            return True
+        else:
+            return False
+
+def logout():
+    del session["user_id"]
+    del session["user_name"]
+    del session["user_role"]
+
+def register(name, password, role):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = """INSERT INTO users (name, password)
+                 VALUES (:name, :password)"""
+        db.session.execute(sql, {"name":name, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return login(name, password)
+
