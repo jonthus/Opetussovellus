e7cf1e561f96457ad5798bceb68b2fed0b6377e0
root <root@LAPTOP-DBRPH672.localdomain>
2021-08-05 16:18:36 +0300

    Updated schema

--- schema.sql
+++ schema.sql
@@ -7,28 +7,6 @@ CREATE TABLE Users (
 CREATE TABLE Courses (
     id SERIAL PRIMARY KEY,
     creator_id INTEGER REFERENCES users,
-    name TEXT,
-    visible INTEGER
+    name TEXT
 );
 
-CREATE TABLE Exercises (
-    id SERIAL PRIMARY KEY,
-    course_id INTEGER REFERENCES courses,
-    content TEXT,
-);
-
-CREATE TABLE Answers (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    exercise_id INTEGER REFERENCES exercises,
-    sent_at TIMESTAMP,
-    result INTEGER
-);
-
-CREATE TABLE Grade (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    course_id INTEGER REFERENCES courses,
-    grade INTEGER,
-    comment TEXT
-);
