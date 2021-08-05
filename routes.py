8ddf683871bd164623f1c05454a7831063d84646
root <root@LAPTOP-DBRPH672.localdomain>
2021-08-05 15:00:49 +0300

    Updated routes

--- routes.py
+++ routes.py
@@ -2,25 +2,11 @@ from app import app
 from db import db
 from flask import Flask
 from flask import redirect, render_template, request, session
-import users, messages
+import users, courses
 
 @app.route("/")
 def index():
-    result = db.session.execute("SELECT content FROM messages")
-    messages = result.fetchall()
-    return render_template("index.html", count=len(messages), messages=messages)
-
-@app.route("/new")
-def new():
-    return render_template("new.html")
-
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(sql, {"content":content})
-    db.session.commit()
-    return redirect("/")
+    return render_template("index.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
