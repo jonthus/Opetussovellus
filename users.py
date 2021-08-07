b13edbb91883062432dfa2e908e8cb941441a514
root <root@LAPTOP-DBRPH672.localdomain>
2021-08-07 15:37:44 +0300

    Updated users

--- users.py
+++ users.py
@@ -3,6 +3,14 @@ from flask import abort, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
 
+def check_role(role):
+    if role > session.get("user_role", 0):
+        return False
+
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        return False
+
 def login(username, password):
     sql = "SELECT password, id, role FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
@@ -13,7 +21,8 @@ def login(username, password):
         hash_value = user.password
         if check_password_hash(hash_value, password):
             session["user_id"] = user[1]
-            session["user_name"] = name
+            session["user_role"] = user[2]
+            session["user_name"] = username
             session["csrf_token"] = os.urandom(16).hex()
             return True
         else:
@@ -24,14 +33,13 @@ def logout():
     del session["user_name"]
     del session["user_role"]
 
-def register(name, password, role):
+def register(username, password, role):
     hash_value = generate_password_hash(password)
     try:
-        sql = """INSERT INTO users (name, password)
-                 VALUES (:name, :password)"""
-        db.session.execute(sql, {"name":name, "password":hash_value})
+        sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, :role)"
+        db.session.execute(sql, {"username":username, "password":hash_value, "role":role})
         db.session.commit()
     except:
         return False
-    return login(name, password)
+    return login(username, password)
 
