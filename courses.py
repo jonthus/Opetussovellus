3153ed71c040815c003f7fba204ddf7d990cc2fa
root <root@LAPTOP-DBRPH672.localdomain>
2021-08-08 20:35:55 +0300

    Updated courses

--- courses.py
+++ courses.py
@@ -1,13 +1,18 @@
 from db import db
 
-def add_course(name, content, user_id):
-    sql = "INSERT INTO courses (user_id, name) VALUES (:user_id, :name)"
-    db.session.execute(sql, {"user_id":user_id, "name":name})
-    sql = "INSERT INTO exercises (exercise_id, content)"
-    db.session.execute(sql, {"exercise_id":exercise_id, "content":content})
-    db.session.commit() 
+def get_list():
+    sql = "SELECT * FROM courses WHERE visible=1"
+    result = db.session.execute(sql)
+    return result.fetchall()
 
-def remove_course(course_id)
-    sql = "UPDATE courses SET VISIBLE=0 WHERE id=:id"
-    db.session.execute(sql, {"id":course_id})
+def add_course(name):
+    sql = "INSERT INTO courses (name, visible) VALUES (:name, 1)"
+    db.session.execute(sql, {"name":name})
     db.session.commit()
+    return 'OK'
+
+def remove_course(name):
+    sql = "UPDATE courses SET VISIBLE=0 WHERE name=:name"
+    db.session.execute(sql, {"name":name})
+    db.session.commit()
+    return 'OK'
